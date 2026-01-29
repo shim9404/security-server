6f138fc0e9190cd1e0a29824ead4aacff14bb738
shim9404 <shim9404@gmail.com>
2026-01-29 13:14:31 +0900

    초기화

---
+++ src/main/java/com/example/security_server/SecurityServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServerApplication.class, args);
+	}
+
+}
